4389021186c3ff9c38d8766cc129b073be385ad3
EdgarB <edgar_daniel_1996@hotmaail.com>
2016-08-28 10:25:28 -0500

    User authentication needed to use framework

--- TurismoHack/config/routes.rb
+++ TurismoHack/config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   resources :servicios_turisticos
   resources :locals
   get 'welcome/index'
-
+  # get 'devise/sessions/new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+   root 'welcome#index'
+  #root 'devise/sessions#new'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
