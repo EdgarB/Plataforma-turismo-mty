74a1760e432298bda21144c98ee4e96150afc68d
EdgarB <edgar_daniel_1996@hotmaail.com>
2016-08-28 02:01:08 -0500

    Nav bar css modified

--- TurismoHack/app/models/local.rb
+++ TurismoHack/app/models/local.rb
@@ -1,2 +1,6 @@
 class Local < ActiveRecord::Base
+  validates :nombre, presence:true
+  validates :apertura, presence:true
+  validates :clausura, presence:true
+  validates :descripcion, presence:true
 end
